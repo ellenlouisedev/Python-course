17cf0a14607161cc6239215a5a30cd1b20eb64ff
ellenlouisedev <ellenlouise767@gmail.com>
2024-10-16 16:55:49 -0300

    atualização

---
+++ cliente.py
@@ -0,0 +1,28 @@
+''' 
+1 - O cliente do banco precisa entrar com os seguintes dados: 
+  Nome, CPF, 
+2 - Mostrar os dados do cliente
+3 - Criar uma lista para salvar os dados do cliente
+'''
+
+nome = input('Digite o nome: ')
+cpf = input('Digite o CPF: ')
+
+cliente = [nome, cpf]
+clientes = [cliente]
+
+nome = input('Digite o nome: ')
+cpf = input('Digite o CPF: ')
+
+cliente = [nome, cpf]
+clientes.append(cliente)
+
+print('-----------------')
+
+print("Clientes cadastrados: ")
+print(f"Nome: {clientes[0][0]}")
+print(f"CPF: {clientes[0][1]}")
+print(f"Nome: {clientes[1][0]}")
+print(f"CPF: {clientes[1][1]}")
+
+print('-----------------')
--- ex01.py
+++ ex01.py
@@ -1,3 +1,12 @@
 x = 5
 y = 2
 print(x + y)
+
+age = 19
+name = 'Ellen'
+print(f'Meu nome é {name} e eu tenho {age} anos de idade')
+
+
+age = 20
+name = 'Ellen'
+print(f'Meu nome é {name} e eu tenho {age} anos de idade')
