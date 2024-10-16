46a48e25de4c8d37e6d4e3db8c9e41ce6a52ecb6
ellenlouisedev <ellenlouise767@gmail.com>
2024-10-16 15:49:47 -0300

    primeiros exercícios

---
+++ ex00.py
@@ -0,0 +1,27 @@
+age = 19
+name = "Ellen"
+
+print (f'meu nome é {name} e eu tenho {age} anos de idade')
+
+lista = [name, age]
+print (lista)
+
+matriz = [
+    ['João', 23],
+    ['Giovana', 27],
+    ['Ellen', 19]
+]
+
+print ('Mostrando a primeira linha:', matriz[0])
+print ('Mostrando a  segunda linha:', matriz[1])
+print ('Mostrando a terceira linha:', matriz[2])
+print ('Mostrando o primeiro item da terceira linha:', matriz [2][0])
+print ('Mostrando todos as linhas', matriz[0], matriz[1], matriz[2])
+
+lista = ["p", "y", "t", "h", "o", "n"]
+
+print (lista [0:])
+print (lista [:2])
+print (lista [1:3])
+print (lista [0:3:2])
+print (lista [-1])
