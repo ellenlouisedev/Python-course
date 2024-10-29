1a82d841285627262e136cfc0b81402746fca184
ellenlouisedev <ellenlouise767@gmail.com>
2024-10-29 15:45:33 -0300

    upadate

--- Cadastro-de-alunos/cadastro_de_alunos.py
+++ Cadastro-de-alunos/cadastro_de_alunos.py
@@ -4,42 +4,63 @@ O sistema deve permitir a inserção, consulta e exclusão de dados dos alunos.
 '''
 
 alunos = {}
-while True:
-    print("\nMenu Principal:")
-    print("1. Cadastrar Aluno")
-    print("2. Consultar Aluno")
-    print("3. Excluir Aluno")
-    print("4. Sair \n")
+def menu():
+    menu_principal = """
+    ==================
+    Menu Principal:
+    1. Cadastrar Aluno
+    2. Consultar Aluno
+    3. Excluir Aluno
+    4. Sair
+    ==================
+    
+    """
+    print(menu_principal)
 
-    opcao = input("Digite sua opção: ")
+def escolher_opcao():
+    while True:
+        opcao = int(input("Digite sua opção"))
+        match(opcao):
+            case 1:
+                cadastrar_aluno()
+            case 2:
+                consultar_aluno()
+            case 3:
+                excluir_aluno()
+            case 4:
+                break
+            case _:
+                print("Opção inválida!")
+        
 
-    if opcao == "1":
-        # Cadastrar aluno: Permitir a inserção de dados do aluno, incluindo nome, matrícula, curso e data de nascimento.
+def cadastrar_aluno():
+    # Cadastrar aluno: Permitir a inserção de dados do aluno, incluindo nome, matrícula, curso e data de nascimento.
         aluno = input("Digite o nome do aluno: ")
         matricula = input("Digite a matrícula do aluno: ")
         curso = input("Digite o curso do aluno: ")
         data_nascimento = input("Digite a data de nascimento do aluno (dd/mm/yyyy): ")
-        print(f"\n Aluno {aluno} cadastrado com sucesso!")
+        print(f"Aluno {aluno} cadastrado com sucesso!")
         alunos[matricula] = {"nome": aluno, "curso": curso, "data_nascimento": data_nascimento}
-        
-    elif opcao == "2":
-        # Buscar um aluno por matrícula e exibir seus dados completos.
+        return alunos
+
+def consultar_aluno():
+    # Buscar um aluno por matrícula e exibir seus dados completos.
         buscar_matricula = input("Digite a matrícula do aluno que deseja consultar: ")
         if buscar_matricula in alunos:
             aluno_encontrado = alunos[buscar_matricula]
-            print(f" \nAluno encontrado: {aluno_encontrado['nome']}")
+            print(f"Aluno encontrado: {aluno_encontrado['nome']}")
             print(f"Matrícula: {buscar_matricula}")
-            print(f"Curso: {aluno_encontrado['curso']}")
-            print(f"Data de Nascimento: {aluno_encontrado['data_nascimento']}")
         else :
             print("Aluno não encontrado!")
-            
-    elif opcao == "3":
-        # Excluir um aluno por matrícula.
-        pass
-    elif opcao == "4":
-        break
-    else:
-        print("Opção inválida!")
 
+def excluir_aluno():
+    # TODO
+    pass
+
+
+menu()
+escolher_opcao()
+# TODO mostrar menu para escolher opções
+
+# TODO criar a função para mostrar os alunos mais organizados
 print(alunos.items())
